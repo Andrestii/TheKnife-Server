165321988af916efac964f075921ef694c5fea92
Andrestii <79668261+Andrestii@users.noreply.github.com.>
2026-01-06 17:00:12 +0100

    Fix loading continuo del server

--- src/main/java/com/theknife/MenuServerController.java
+++ src/main/java/com/theknife/MenuServerController.java
@@ -16,38 +16,29 @@ public class MenuServerController {
     @FXML
     TextField hostField, usernameField, passwordField;
 
-    public void invioDatiDB(){
+    public void invioDatiDB() {
         String hostDB = hostField.getText();
         String usernameDB = usernameField.getText();
         String passwordDB = passwordField.getText();
-
         database = new Database(hostDB, usernameDB, passwordDB);
-        
-        try {
-            ss = new ServerSocket(PORT);
-            System.out.println("Server ready");
-            while (true) {
-                Socket cliSocket = ss.accept();
-                System.out.println("Client connected: socket = " + cliSocket);
-                new Thread(new ServerThread(cliSocket, database)).start();
-                //cliSocket.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (ss != null) {
-                    ss.close();
+
+        Thread serverLoop = new Thread(() -> {
+            try (ServerSocket ss = new ServerSocket(PORT)) {
+                System.out.println("Server ready");
+                while (true) {
+                    Socket cliSocket = ss.accept();
+                    System.out.println("Client connected: socket = " + cliSocket);
+                    new Thread(new ServerThread(cliSocket, database)).start();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-       
+        });
+        serverLoop.setDaemon(true);
+        serverLoop.start();
     }
-    
 
     public static void main(String[] args) {
-        
+
     }
 }
